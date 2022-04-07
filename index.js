a856c29a5cf8d28d0d0a896811e2203f488c08ed
Eric Xiao <es.xiao93@gmail.com>
2022-04-07 09:43:17 -0400

    commit tree 4c9dfd42e000efe223c5319914fcc8592a2ccfb9

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let charge;
+  if (distance <= 400) {
+    charge = 'This one is on me!';
+  } else if (distance > 400 && distance < 2000) {
+    charge = 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance < 2500) {
+    charge = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    charge = 'No can do.';
+  }
+  return charge;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+if (tip === "generous") {
+  return "Thank you so much.";
+} else if (tip === "not as generous") {
+      return "Thank you.";
+} else {
+  return "Bye.";
+}
 }
